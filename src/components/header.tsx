5806117f5c59debb2f19ca64d1971cb4ae6475bb
celemar <celemar@pm.me>
2024-06-22 11:23:27 -0300

    first commit

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <header className="bg-white dark:bg-[#2b3743] text-center sticky top-0 shadow-[0_4px_6px_-1px_rgba(0,0,0,0.1),0_2px_4px_-1px_rgba(0,0,0,0.06)]">
+    <header className="bg-white dark:bg-[#2b3743] text-center sticky top-0 shadow-[0_2px_6px_-1px_rgba(0,0,0,0.1),0_2px_2px_-1px_rgba(0,0,0,0.06)]">
       <nav className="flex justify-between p-8 xl:px-16 mx-auto dark:text-[#fff] text-sm md:text-base max-w-[1440px]">
         <Link href="/">
           <h1 className="font-extrabold text-base md:text-xl">
